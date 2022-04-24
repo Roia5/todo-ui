1f20a8d15383126a4d62b97e954430dce1c38c43
Roia5 <roeyash5@gmail.com>
2022-04-24 10:30:11 +0000

    a

--- src/TaskCreate.js
+++ src/TaskCreate.js
@@ -18,7 +18,7 @@ export default () => {
       <div>
         <form onSubmit={onSubmit}>
             <div className="form-group">
-                <label>What needs to be done?</label>
+                <label>What needs to be done? </label>
                 <input value={content} 
                 onChange={e => setContent(e.target.value)} 
                 className="form-control" />
